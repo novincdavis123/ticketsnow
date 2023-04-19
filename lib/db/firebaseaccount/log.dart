ee8f15169d49375901801e14e71ffa7e64f214a5
novincdavis123 <novindavisks@gmail.com>
2023-04-19 21:30:42 +0530

    commit

--- lib/db/firebaseaccount/log.dart
+++ lib/db/firebaseaccount/log.dart
@@ -18,27 +18,69 @@ class _FirelogState extends State<Firelog> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          TextField(controller: email),
-          TextField(controller: pass),
-          ElevatedButton(
-              onPressed: () {
-                String eemail = email.text.trim();
-                String pwd = pass.text.trim();
-                AuthHelper()
-                    .signIn(email: eemail, password: pwd)
-                    .then((result) {
-                  if (result == null) {
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) => Welcome()));
-                  } else {
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: Text(result)));
-                  }
-                });
-                email.text = '';
-                pass.text = '';
-              },
-              child: Text('Login')),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 100,
+              width: 300,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          'https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y29tcHV0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'))),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+                controller: email,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide:
+                          BorderSide(width: 1, color: Colors.pink.shade100)),
+                  filled: true,
+                  fillColor: Colors.grey,
+                  hintStyle: TextStyle(color: Colors.pink.shade100),
+                  hintText: "email:",
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+                controller: pass,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide:
+                          BorderSide(width: 1, color: Colors.pink.shade100)),
+                  filled: true,
+                  fillColor: Colors.grey,
+                  hintStyle: TextStyle(color: Colors.pink.shade100),
+                  hintText: "Password:",
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                onPressed: () {
+                  String eemail = email.text.trim();
+                  String pwd = pass.text.trim();
+                  AuthHelper()
+                      .signIn(email: eemail, password: pwd)
+                      .then((result) {
+                    if (result == null) {
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) => Welcome()));
+                    } else {
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text(result)));
+                    }
+                  });
+                  email.text = '';
+                  pass.text = '';
+                },
+                child: Text('Login')),
+          ),
           TextButton(
               onPressed: () {
                 Navigator.push(
--- lib/db/firebaseaccount/signup.dart
+++ lib/db/firebaseaccount/signup.dart
@@ -15,26 +15,69 @@ class _LogState extends State<Log> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          TextField(controller: email),
-          TextField(controller: pass),
-          ElevatedButton(
-              onPressed: () {
-                String mail = email.text.toString();
-                String pwd = pass.text.toString();
-                AuthHelper().signup(email: mail, password: pwd).then((result) {
-                  if (result == null) {
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) => Firelog()));
-                  } else {
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: Text(result)));
-                  }
-                });
-                email.text = '';
-                pass.text = '';
-              },
-              child: Text('Sign up'))
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 100,
+              width: 300,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y29tcHV0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'))),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+                controller: email,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide:
+                          BorderSide(width: 1, color: Colors.lightBlueAccent)),
+                  filled: true,
+                  fillColor: Colors.grey,
+                  hintStyle: TextStyle(color: Colors.lightBlueAccent),
+                  hintText: "email:",
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+                controller: pass,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide:
+                          BorderSide(width: 1, color: Colors.lightBlueAccent)),
+                  filled: true,
+                  fillColor: Colors.grey,
+                  hintStyle: TextStyle(color: Colors.lightBlueAccent),
+                  hintText: "password:",
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                onPressed: () {
+                  String mail = email.text.toString();
+                  String pwd = pass.text.toString();
+                  AuthHelper().signup(email: mail, password: pwd).then((result) {
+                    if (result == null) {
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) => Firelog()));
+                    } else {
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text(result)));
+                    }
+                  });
+                  email.text = '';
+                  pass.text = '';
+                },
+                child: Text('Sign up')),
+          )
         ],
       ),
     );
